b1533085423e1d6f6b9adf33cbc4b92962aa9e7a
Remy Dufay <remy.dufay@gmail.com>
2022-05-31 16:45:41 +0200

    Routes des sessions song a faire

--- app/controllers/game_sessions_controller.rb
+++ app/controllers/game_sessions_controller.rb
@@ -9,40 +9,43 @@ class GameSessionsController < ApplicationController
 
   def show
     @game_session = GameSession.find(params[:id])
-
+    @lyrics = @game_session.game_session_songs[0].song.lyrics
 
     # Récupération des paroles
-    # $htmlyrics = @game_session.songs[0].lyrics
+    #$htmlyrics = @game_session.songs[0].lyrics
 
     # Transformation des paroles en module Nokogiri, qui permettra par la suite
     # de naviguer dans les balises html et censurer les mots (sans perdre le balisage)
     #@html = Nokogiri::HTML.fragment($htmlyrics)
 
     # On stocke le titre (la premier balise div) dans un coin pour vérifier la condition de victoire
-    @title = @game_session.game_session_songs[0].game_song.song.title
+    #@title = @game_session.songs[0].title
 
     # Création d'un array avec tous les mots de la chanson, qui permet ensuite de
     # lancer un .count et récupérer la fréquence d'un mot dans la chanson
-    @arrayofwords =
+    #@arrayofwords = $htmlyrics.split(/([a-zA-Z\u00C0-\u00FF]+|\s|\W|\w\W\w)/).map!(&:downcase).reject!(&:empty?)
 
     # A chaque chargement de la page, on génére les paroles censurées :
-    @htmlredact = htmlredact()
+    #@htmlredact = htmlredact()
 
     # On vérifie si il ya un mot soumis par le formulaire
     # Si oui on crée un Guess avec, et on redirect vers le show pour "nettoyer" l'url du param
     # Peut sans doute etre amélioré en ne mettant pas le résultat du form dans l'url ?
     # Ce qui éviterait une redirection inutile.
-    if params[:query].present?
-      word = params[:query].downcase.strip
-      frequency  = @arrayofwords.count(word)
-      Guess.create!(game_session: @game_session, word: word, frequency: frequency)
-      redirect_to game_session_path(@game_session)
-    end
+    #if params[:query].present?
+    #  word = params[:query].downcase.strip
+    #  frequency  = @arrayofwords.count(word)
+    #  Guess.create!(game_session: @game_session, word: word, frequency: frequency)
+     # redirect_to game_session_path(@game_session)
+    #end
 
     # Condition de victoire : si le texte ne censure plus le titre, c'est gagné.
-    if @htmlredact.include? @title
-      flash[:notice] = "✨Bravo !✨"
-    end
+   # if @htmlredact.include? @title
+     #flash[:notice] = "✨Bravo !✨"
+    #end
+
+
+
   end
 
   def all
@@ -56,8 +59,8 @@ private
 # C'est Nokogiri qui fait le job de visiter chaque div.
 # Pourquoi ne pas itérer et renvoyer du texte ? Parce qu'on veut garder
 # les balises html pour la mise en forme !
-  def htmlredact
-    htmlredacted = @html.css("div").each do |node|
+  def htmlredact(html)
+    htmlredacted = html.css("div").each do |node|
       node.content = stringredact(node.content)
     end
     return htmlredacted.to_html
