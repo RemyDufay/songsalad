d755d9b8c0472fe379876126374018b709f84e48
Remy Dufay <remy.dufay@gmail.com>
2022-06-03 12:03:38 +0200

    Retouche home + regles + victoryscreen (bouton copie OK)

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -3,12 +3,13 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   static targets = ["input"];
   static values = {
-    feedbackText: String
+    feedbackText: String,
+    copied: String
   }
 
   copy(event) {
-    this.inputTarget.select();
-    document.execCommand('copy');
+
+    navigator.clipboard.writeText(this.copiedValue);
     event.currentTarget.disabled = true;
     event.currentTarget.innerText = this.feedbackTextValue;
   }
