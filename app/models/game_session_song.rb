f8c6309a45adab720939a22a150b477bf2ac976c
Valentin Chauveau <valentin.korea@gmail.com>
2022-06-06 12:09:48 +0200

    front-game-and-stats-val

--- app/models/game_session_song.rb
+++ app/models/game_session_song.rb
@@ -39,11 +39,9 @@ private
 def redact(word)
   redacted = ""
   word.each_char do |char|
-    (char =~ /[[:alpha:]]/) ? redacted += "█" : redacted += char
+    (char =~ /[[:alpha:]]/) ? redacted += "&#95;" : redacted += char
   end
   return redacted = "<span>#{redacted}</span>"
 end
-
-
 
 end
