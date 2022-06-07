9cbb03dc8de547b80ecaa6e4abeb3e11eeec6125
Remy Dufay <remy.dufay@gmail.com>
2022-06-07 21:32:25 +0200

    premiere anim ok

--- app/javascript/controllers/home_controller.js
+++ app/javascript/controllers/home_controller.js
@@ -1,9 +1,22 @@
 import { Controller } from "stimulus"
+import Rails from "@rails/ujs";
+
+
 
 export default class extends Controller {
   static targets = [ "home" ]
 
-  out() {
-    this.homeTarget.classList.add("animate__animated", "animate__fadeOutRight")
+  out(event) {
+    event.preventDefault();
+    console.log("stop !")
+    var self = this;
+    this.homeTarget.classList.add("animate__animated", "animate__backOutRight")
+    setTimeout(gotosession, 450)
   }
+
+}
+
+
+function gotosession() {
+  Rails.ajax({url: '/games/3/game_sessions',type: 'POST'});
 }
