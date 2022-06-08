3a127a15ca43840b67ede2b2f41152864d990145
Valentin Chauveau <valentin.korea@gmail.com>
2022-06-08 10:46:31 +0200

    add a friend done

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -7,21 +7,25 @@ class FriendshipsController < ApplicationController
   end
 
   def create
-    @friendship = Friendship.create(friendship_params)
-    @friendship.status = "pending"
-    @friendship.save!
-    redirect_to friendships_path
-
-  end
-
-  def user_index
-    @users = User.all
-    @search = params["search"]
-    if @search.present?
-      @name = @search["name"]
-      @friend = User.where(name: @name)
+    params[:friendship][:friend_id].reject(&:empty?).each do |friend_id| # ["3", "5"]
+      @friendship =Friendship.new({
+        user: current_user,
+        friend: User.find(friend_id),
+        status: "pending"
+      })
+      @friendship.save!
     end
+    redirect_to friendships_path
   end
+
+  # def user_index
+  #   @users = User.all
+  #   @search = params["search"]
+  #   if @search.present?
+  #     @name = @search["name"]
+  #     @friend = User.where(name: @name)
+  #   end
+  # end
 
   def accept
     @friendship = Friendship.find(params[:id])
