65638b373258ad499d5a5cf66c3c6064174d4c2f
antoine manceau <antoine.mgy.mnc@gmail.com>
2022-06-09 13:20:13 +0200

    error redirection unless front

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module Songsalad
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.action_view.automatically_disable_submit_tag = false
+    config.exceptions_app = self.routes
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
   devise_for :users
   root to: 'pages#home'
-
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 
 resources :games, only: [] do
   resources :game_sessions, only: [:create, :show] do
